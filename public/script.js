a6bbf29e97ca5bcbc54f265bf51af25879dfbae0
Aditya Thorat <thorat.aditya@outlook.com>
2022-11-24 00:53:23 +0530

    add simultaneous editing

--- public/script.js
+++ public/script.js
@@ -3,8 +3,32 @@ window.onload = function () {
     var pad = document.getElementById('pad');
     var markdownArea = document.getElementById('markdown');
 
+    // make the tab act like a tab
+    pad.addEventListener('keydown', function (e) {
+        if (e.keyCode === 9) { // tab was pressed
+            // get caret position/selection
+            var start = this.selectionStart;
+            var end = this.selectionEnd;
+
+            var target = e.target;
+            var value = target.value;
+
+            // set textarea value to: text before caret + tab + text after caret
+            target.value = value.substring(0, start)
+                + "\t"
+                + value.substring(end);
+
+            // put caret at right position again (add one for the tab)
+            this.selectionStart = this.selectionEnd = start + 1;
+
+            // prevent the focus lose
+            e.preventDefault();
+        }
+    });
+
     var previousMarkdownValue;
 
+    // convert text area to markdown html
     var convertTextAreaToMarkdown = function () {
         var markdownText = pad.value;
         previousMarkdownValue = markdownText;
@@ -19,28 +43,26 @@ window.onload = function () {
         return false;
     };
 
+    // check every second if the text area has changed
     setInterval(function () {
         if (didChangeOccur()) {
             convertTextAreaToMarkdown();
         }
     }, 1000);
 
+    // convert textarea on input change
     pad.addEventListener('input', convertTextAreaToMarkdown);
 
-    sharejs.open(document.location.pathname, 'text', function (error, doc) {
-        doc.attach_textarea(pad);
-
     // ignore if on home page
-    if(document.location.pathname.length > 1){
+    if (document.location.pathname.length > 1) {
         // implement share js
         var documentName = document.location.pathname.substring(1);
-        sharejs.open(documentName, 'text', function(error, doc) {
+        sharejs.open(documentName, 'text', function (error, doc) {
             doc.attach_textarea(pad);
             convertTextAreaToMarkdown();
         });
     }
 
-        
-        convertTextAreaToMarkdown();
-    });
+    // convert on page load
+    convertTextAreaToMarkdown();
 };
