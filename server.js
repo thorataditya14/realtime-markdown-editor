cc13c97178fc5ea55b71c2d533ce1495bf9b6d2f
Aditya Thorat <thorat.aditya@outlook.com>
2022-11-25 13:16:57 +0530

    connect to redis instance

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+const Redis = require('ioredis');
+require('dotenv').config();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -11,6 +13,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
     res.render('pad');
 });
+
 app.get('/(:id)', function (req, res) {
     res.render('pad');
 });
@@ -19,15 +22,17 @@ app.get('/(:id)', function (req, res) {
 var sharejs = require('share');
 
 // set up redis server
-var redisClient;
-console.log(process.env.REDISTOGO_URL);
-if (process.env.REDISTOGO_URL) {
-    var rtg = require("url").parse(process.env.REDISTOGO_URL);
-    redisClient = require("redis").createClient(rtg.port, rtg.hostname);
-    redisClient.auth(rtg.auth.split(":")[1]);
-} else {
-    redisClient = require("redis").createClient();
-}
+const redisClient = new Redis({
+    port: process.env.REDIS_PORT,
+    host: process.env.REDIS_HOST,
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
+});
+
+// check connection
+redisClient.on("connect", () => {
+    console.log("Redis Connected")
+})
 
 // options for sharejs
 var options = {
